18e71d9d5ccc0be7f3eb35617e6e1ec3f4f987e3
Peetz0r <peter@haas-en-berg.nl>
2023-11-06 14:03:20 +0100

    meâh invoeâhmaugelijkheide

--- harrie.py
+++ harrie.py
@@ -1,30 +1,54 @@
 #!/bin/env python
 
-import os, sys, re
+import os, sys, re, readline
 
-Q42FILE = './HaagsTranslator/src/HaagsTranslator/Translator.cs'
-Q42FILE = os.path.realpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), Q42FILE))
+try:
 
-reRe = re.compile(r'new *\[\] *\{ *"([^"]+)" *, *"([^"]+)" *\}')
-reBackref = re.compile(r'\$([0-9]+)')
+  Q42FILE = './HaagsTranslator/src/HaagsTranslator/Translator.cs'
+  Q42FILE = os.path.realpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), Q42FILE))
 
-if not os.path.exists(Q42FILE):
-  print(f'Translation file missing: {Q42FILE}')
-  print(f'You can find (and update) it with: git clone git@github.com:Q42/HaagsTranslator.git')
-  sys.exit(42)
+  PROMPT = 'harrie> '
 
-reList = []
+  reRe = re.compile(r'new *\[\] *\{ *"([^"]+)" *, *"([^"]+)" *\}')
+  reBackref = re.compile(r'\$([0-9]+)')
 
-with open(Q42FILE) as f:
-  for line in f.readlines():
-    match = reRe.search(line)
-    if match:
-      backref = reBackref.sub(r'\\\1', match.group(2))
-      reList.append((re.compile(match.group(1)), backref))
+  if not os.path.exists(Q42FILE):
+    print(f'Translation file missing: {Q42FILE}')
+    print(f'You can find (and update) it with: git clone git@github.com:Q42/HaagsTranslator.git')
+    sys.exit(42)
 
-text = sys.stdin.read()
+  reList = []
 
-for i in reList:
-  text = i[0].sub(i[1], text)
+  with open(Q42FILE) as f:
+    for line in f.readlines():
+      match = reRe.search(line)
+      if match:
+        backref = reBackref.sub(r'\\\1', match.group(2))
+        reList.append((re.compile(match.group(1)), backref))
 
-print(text)
+  loop = False
+
+  if os.isatty(0) is False:
+    text = sys.stdin.read()
+  elif len(sys.argv) > 1:
+    text = sys.argv[1]
+  else:
+    text = input(PROMPT)
+    loop = True
+
+  while True:
+    text = text.strip()
+
+    for i in reList:
+      text = i[0].sub(i[1], text)
+
+    print(text)
+
+    if not loop:
+      break
+
+    text = input(PROMPT)
+
+except (KeyboardInterrupt, EOFError):
+  print()
+  sys.exit(0)
